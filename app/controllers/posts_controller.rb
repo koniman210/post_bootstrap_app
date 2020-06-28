e5271c2e3174f1f9c4d689f893b2a1971f87f017
koniman1123@gmail.com <>
2020-06-29 03:36:35 +0900

    フラッシュの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,22 +15,22 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create!(post_params)
-    redirect_to post
+    redirect_to post ,notice: "投稿しました"
   end
 
   def update
   end
 
   def destroy
-    post.destroy!
-    redirect_to post
+    @post.destroy!
+    redirect_to @post, alert: "削除しました"
   end
   def edit
   end
 
   def update
-    post.update!(post_params)
-    redirect_to post
+    @post.update!(post_params)
+    redirect_to @post, notice: "更新しました"
   end
 
   private
