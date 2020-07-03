33636c8376dc366f39c63a8ed6c0025a11a61b76
koniman1123@gmail.com <>
2020-07-03 18:17:36 +0900

    完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,7 +51,7 @@ class PostsController < ApplicationController
     end
 
     def post_params
-      params.require(:post).permit(:title,:content)
+      params.require(:post).permit(:title,:content,:image)
     end
 
 end
